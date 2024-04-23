acbcf3d6876d593e93eca7d259dcf97e0cc2b7a5
Ode Ardika <dirfanardika@gmail.com>
2024-04-23 15:36:05 +0800

    feat(Header): Make the header responsive and add burger button on navbar

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import style from './Header.module.css'
 
 export default function Header() {
     const navigation = [{id : 1, placeholder : "Profile", href : "#profile",},
                         {id : 2, placeholder : "Project", href : "#project"},
                         {id : 3, placeholder : "Achievement", href : "#achievement"}];
-                        
+    const [isNavOpen, setIsNavOpen] = useState(false)
+    
+    function handleIsNavOpen(event){
+      setIsNavOpen(prevIsNavOpen => !prevIsNavOpen )
+    }
+    
   return (
     <header className={style.header}>
         <div className={style.header_content}>
           <h1 className={style.title}>
             <u>Ode Ardika</u>
           </h1>
-          <nav classNameName="navigation" id="navigation">
-            <div className={style.nav_button}>
-              <hr className="top" />
-              <hr className="mid" />
-              <hr className="bottom" />
+          <nav className={isNavOpen? style.navigation_open : style.navigation} id="navigation">
+            <div className={style.nav_button} onClick={handleIsNavOpen}>
+              <hr className={style.top} />
+              <hr className={style.mid} />
+              <hr className={style.bottom} />
             </div>
-            <ul className={style.navigation_list} id="navigation-list">
+            <ul className={isNavOpen? style.navigation_list_open : style.navigation_list} id="navigation-list">
                 {navigation.map(li => <li key={li.id}><a href={li.href}>{li.placeholder}</a></li>)}
             </ul>
           </nav>
