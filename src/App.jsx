e116d40a6732f40ffe850858b4e8be377f3e5b3e
Ode Ardika <dirfanardika@gmail.com>
2024-04-28 22:30:16 +0800

    feat(SocialMedia): Create aside section for social media

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./component/Footer/Footer";
 import Profile from "./component/Profile/Profile";
 import Project from "./component/Project/Project";
 import Certificate from "./component/Certificate/Certificate";
+import SosMed from "./component/SosMed/SosMed";
 
 
 export default function App() {
@@ -16,27 +17,7 @@ export default function App() {
             <Project/>
             <Certificate />
         </div>
-        {/* <aside>
-            <div className="social-media-icon">
-                <ul>
-                    <li className="github-icon">
-                        <a href="https://github.com/odeardika">
-                        <img src="assets/img/icon/github.svg" alt="github icon" srcset=""/>
-                        </a>
-                    </li>
-                    <li className="linkedin-icon">
-                        <a href="https://www.linkedin.com/in/putu-ode-irfan-ardika-178373275/">
-                        <img src="assets/img/icon/linkedin.svg" alt="linkedin"/>
-                        </a>
-                    </li>
-                    <li className="instagram-icon">
-                        <a href="https://www.instagram.com/odeardika/">
-                        <img src="assets/img/icon/instagram.svg" alt="instagram"/>
-                        </a>
-                    </li>
-                </ul>   
-            </div>
-        </aside> */}
+        <SosMed/>
     </main>
     
     <Footer/>
---
+++ src/component/SosMed/SosMed.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import style from './SosMed.module.css'
+import githubImg from '../../assets/icon/github.svg';
+import instagramImg from '../../assets/icon/instagram.svg';
+import lingkedinImg from '../../assets/icon/linkedin.svg';
+
+
+export default function SosMed() {
+    const listIcon = [{id : 1,img : githubImg, alt : "github", href : "https://github.com/odeardika"},
+                      {id : 2,img : lingkedinImg, alt : "linkedin", href : "https://www.linkedin.com/in/putu-ode-irfan-ardika-178373275/"},
+                      {id : 3,img : instagramImg, alt : "instagram", href : "https://www.instagram.com/odeardika/"}]
+  
+    return (
+    <>
+    <aside className={style.sosial_media}>
+        <div className={style.icon}>
+            <ul>
+                {listIcon.map(icon => <li key={icon.id}>
+                    <a href={icon.href} target="_blank">
+                        <img src={icon.img} alt={icon.alt} />
+                    </a>
+                </li>)}
+            </ul>   
+        </div>
+    </aside>
+    </>
+  )
+}
